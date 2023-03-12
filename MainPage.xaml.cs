e9195ba538820d287f5eea0f46305cc31ba0005b
Mujahid Islami Primaldi Abdullah <mujahidislami030@gmail.com>
2023-03-12 21:51:41 +0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMaker;
+
+public partial class MainPage : ContentPage
+{
+	bool isRandom;
+	string hexValue;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Slider_ValueChanged(object sender, EventArgs e)
+	{
+		if(!isRandom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+	}
+
+    private void SetColor(Color color)
+    {
+        btnRandom.BackgroundColor= color;
+		Container.BackgroundColor = color;
+		hexValue = color.ToHex();
+		lblHex.Text = hexValue;
+    }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+		isRandom= true;
+		var random = new Random();
+
+		var color = Color.FromRgb(
+			random.Next(0, 265),
+			random.Next(0, 265),
+			random.Next(0, 265)
+			);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+
+		SetColor(color);
+		isRandom = false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		await Clipboard.SetTextAsync(hexValue);
+		var toast = Toast.Make("Copied!", CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+
+		await toast.Show();
+    }
+}
+
